0a4658ad9fb0bf040ff4a186f4cb29d5b59613b8
Morticzek <tahion123@gmail.com>
2022-04-17 20:18:03 +0200

    Adding Vue.js framework files configured with Django REST framework

---
+++ PawTravel/pawtravel-front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SamplePage msg="Welcome to PawTravel"/>
+</template>
+
+<script>
+import SamplePage from './components/SamplePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SamplePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ PawTravel/users/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+# Create your serializers here.
+
+# Serializer for CustomUser model
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'password', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions')
+        read_only_fields = ('id', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions')
