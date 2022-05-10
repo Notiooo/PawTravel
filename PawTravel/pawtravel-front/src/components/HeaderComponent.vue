61509d4e39ada1e0613c9bbb64cfd536b68ec85d
Morticzek <tahion123@gmail.com>
2022-05-10 11:05:45 +0200

    Added header and footer components

--- PawTravel/pawtravel-front/src/components/HeaderComponent.vue
+++ PawTravel/pawtravel-front/src/components/HeaderComponent.vue
@@ -1,20 +1,58 @@
 <template>
   <header>
-    <nav class="uk-navbar-container">
-      <div class="uk-navbar-left">
-        <ul class="uk-navbar-nav">
-          <li class="pawtravel-icon">
-          </li>
-          <li>
-            Test
-          </li>
-        </ul>
+    <div class="uk-grid">
+      <!--<div class="uk-width-1-6"/> -->
+      <div class="uk-width-5-6 uk-align-center">
+        <nav class="uk-navbar-container uk-margin uk-navbar">
+          <div class="uk-navbar-left">
+            <div class="uk-navbar-item">
+              <a class="uk-navbar-item uk-logo">
+                <img src="../assets/static/img/PawTravel-icon.png" alt="PawTravel">
+                PawTravel
+              </a>
+            </div>
+            </div>
+          <div class="uk-navbar-center">
+            <div class="uk-navbar-item">
+              <form action="javascript:void(0)">
+                <input class="uk-input uk-form-width-large m-font pk-button-primary" type="text" placeholder="Lorem Ipsum">
+              </form>
+            </div>
+          </div>
+              <div class="uk-navbar-right">
+            <div class="uk-navbar-item">
+              <button class="uk-button uk-button-default pk-button-primary">
+                <span uk-icon="icon: check"></span>Add</button>
+            </div>
+            <div class="uk-navbar-item">
+              <a class="uk-icon-link">
+                <img src="../assets/static/img/Notification_icon.png" alt="Notification">
+              </a>
+            </div>
+            <div class="uk-navbar-item">
+              <a class="uk-icon-link">
+                <img src="../assets/static/img/Urgent_Message_icon.png" alt="Urgent Message">
+              </a>
+            </div>
+            <div class="uk-navbar-item">
+              <a class="uk-icon-link">
+                <img src="../assets/static/img/User_icon.png" alt="User">
+              </a>
+              </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+<!--      <div class="uk-width-1-6"/>-->
+    </div>
   </header>
 </template>
-
 <script>
+
+import 'uikit'
+import 'uikit/dist/css/uikit.min.css'
+import 'uikit/dist/js/uikit.min.js'
+import 'uikit/dist/js/uikit-icons.min.js'
+
 export default {
   data () {
     return {
@@ -25,5 +63,6 @@ export default {
 </script>
 
 <style scoped>
-@import './../assets/static/css/uikit.min.css';
+@import 'uikit/dist/css/uikit.min.css';
+@import './../assets/static/css/pawtravel.css';
 </style>
--- PawTravel/pawtravel-front/src/main.js
+++ PawTravel/pawtravel-front/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons.js';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+// UIkit.notification('Hello world.');
+
 
 createApp(App).use(router).mount('#app')
