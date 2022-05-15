0b575cf0858dce5ab290f60d2a38e24a304f5126
Notiooo <dawidgrobert@gmail.com>
2022-05-15 12:36:52 +0200

    Minor syntax fix

--- PawTravel/users/models.py
+++ PawTravel/users/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    aboutMe = models.CharField(max_length=100, blank=True),
+    about_me = models.CharField(max_length=100, blank=True),
     email = models.EmailField(unique=True)
