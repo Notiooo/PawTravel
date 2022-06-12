aa71e05f297d45b7e54a4531cb54861d1134886e
Notiooo <dawidgrobert@gmail.com>
2022-06-13 00:00:07 +0200

    Fix unit tests, and add some cosmetics to frontend

--- PawTravel/comments/tests.py
+++ PawTravel/comments/tests.py
@@ -15,7 +15,9 @@ class CommentsTestCase(TestCase):
         """Inserts the necessary test objects into the database"""
 
         mock_file = tempfile.NamedTemporaryFile(suffix=".jpg").name
-        user = CustomUser.objects.create(username='user1', password='password')
+        user = CustomUser.objects.create(username='user1', password='password', email='user1@gmail.com')
+        user.set_password('password')
+        user.save()
         OfferCategory.objects.create(name="TestCategory")
         Offer.objects.create(
             title="TestTitle",
@@ -38,23 +40,39 @@ class DetailOfferLoggedInCommentsTestCase(CommentsTestCase):
     """
 
     def setUp(self):
-        super().setUp()
+        super(DetailOfferLoggedInCommentsTestCase, self).setUp()
         self.client.login(username='user1', password='password')
 
     def test_textarea_placeholder(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
         self.assertContains(response, 'Tell us what you think')
 
     def test_comment_button(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
         self.assertContains(response, 'Send a comment')
+
+
+class DetailOfferLoggedOutCommentsTestCase(CommentsTestCase):
+    """
+    The case where the user is logged in and we check if he has the
+    capabilities available to the logged in user
+    """
+
+    def setUp(self):
+        super(DetailOfferLoggedOutCommentsTestCase, self).setUp()
+
+    def test_(self):
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
+        self.assertContains(response, 'You may write your own comment! '
+                                      'All you have to do is '
+                                      '<a href="/users/login/?next=/offers/testtitle-1/">sign in</a>')
 
 
 class DetailOfferWithoutCommentsTestCase(CommentsTestCase):
     """A case to check the correct behavior when there are no comments"""
 
     def test_no_comments(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
         self.assertContains(response, '0 comments')
 
 
@@ -79,17 +97,17 @@ class DetailOfferWithCommentsTestCase(CommentsTestCase):
         self.comment.save()
 
     def test_one_comment(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
-        self.assertContains(response, '1 comments')
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
+        self.assertContains(response, '1 comment')
 
     def test_correct_user(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
         self.assertContains(response, 'user2')
 
     def test_correct_comment_text(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
         self.assertContains(response, 'Test comment content')
 
     def test_correct_date(self):
-        response = self.client.get(reverse('offer', kwargs={'pk': 1}))
+        response = self.client.get(reverse('offer', kwargs={'pk': 1, 'slug_url': 'testtitle'}))
         self.assertContains(response, 'May 17, 2020, 4:50 p.m.')
