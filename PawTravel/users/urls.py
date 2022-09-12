42ef9b87e4222f69743dfaa190f19fb6f39e5a56
JuliaBoczkowska <56159677+JuliaBoczkowska@users.noreply.github.com>
2022-09-12 22:19:44 +0200

    Add web design of the user profile - close #43

--- PawTravel/users/urls.py
+++ PawTravel/users/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('profile_added/', views.ProfileAddedView.as_view(), name='profile_added'),
+    path('profile_activities/', views.ProfileActivitiesView.as_view(), name='profile_activities'),
     path('social-auth/', include('social_django.urls', namespace='social'))
 ]
--- PawTravel/users/views.py
+++ PawTravel/users/views.py
@@ -16,3 +16,21 @@ class RegisterView(generic.CreateView):
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by('id')
     serializer_class = CustomUserSerializer
+
+
+class SignUpView(generic.CreateView):
+    template_name = 'users/signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+
+
+class ProfileActivitiesView(generic.CreateView):
+    template_name = 'users/user_profile_activities.html'
+    model = CustomUser
+    fields = '__all__'
+
+
+class ProfileAddedView(generic.CreateView):
+    template_name = 'users/user_profile_added.html'
+    model = CustomUser
+    fields = '__all__'
